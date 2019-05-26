07dd640c5760148b31a38cf6a1a1cc5a07ae5421
David Kimura <dkimura@pivotal.io>
2019-05-25 21:17:18 -0700

    Initial preprocessor work

--- scanner.c
+++ scanner.c
@@ -3,6 +3,18 @@
 void
 do_preprocessing(char *infile, char *outfile)
 {
+    char *in_content;
+    char *out_content;
+    int in_index;
+    int out_index;
+
+    /*
+     * Find macros and includes inside infile in order to construct outfile
+     * replacing  macros and includes
+     */
+    for (;;)
+    {
+    }
 }
 
 struct token *
