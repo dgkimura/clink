8e0bec3ccc118296dcd87e513313bfd387cd9b25
David Kimura <dkimura@pivotal.io>
2019-07-18 18:08:11 -0700

    Add initial parser files

---
+++ src/parser.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "parser.h"
+
+struct astnode *
+do_parsing(struct listnode *tokens)
+{
+    return NULL;
+}
---
+++ src/parser.h
@@ -0,0 +1,8 @@
+#include "scanner.h"
+
+struct astnode
+{
+};
+
+struct astnode *
+do_parsing(struct listnode *tokens);
