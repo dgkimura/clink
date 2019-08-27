21566e941c6c1db8bacb7c399a5b70009ddc4c67
David Kimura <dkimura@pivotal.io>
2019-08-27 13:10:33 -0700

    Separate terminal/non-terminal grammar symbols

--- src/parser.h
+++ src/parser.h
@@ -5,7 +5,72 @@
 
 enum astnode_t
 {
-    AST_INVALID = -1,
+    /*
+     * following are terminal values in grammar
+     */
+    AST_IDENTIFIER,
+    AST_ARROW,
+    AST_PLUS,
+    AST_PLUS_PLUS,
+    AST_PLUS_EQUAL,
+    AST_MINUS,
+    AST_MINUS_MINUS,
+    AST_MINUS_EQUAL,
+    AST_AMPERSAND,
+    AST_AMPERSAND_AMPERSAND,
+    AST_ASTERISK,
+    AST_ASTERISK_EQUAL,
+    AST_BACKSLASH,
+    AST_BACKSLASH_EQUAL,
+    AST_CARET,
+    AST_COMMA,
+    AST_MOD,
+    AST_MOD_EQUAL,
+    AST_QUESTIONMARK,
+    AST_COLON,
+    AST_SEMICOLON,
+    AST_LPAREN,
+    AST_RPAREN,
+    AST_LBRACKET,
+    AST_RBRACKET,
+    AST_LBRACE,
+    AST_RBRACE,
+    AST_VERTICALBAR,
+    AST_VERTICALBAR_VERTICALBAR,
+    AST_SHIFTLEFT,
+    AST_SHIFTRIGHT,
+    AST_LT,
+    AST_GT,
+    AST_LTEQ,
+    AST_GTEQ,
+    AST_EQ,
+    AST_NEQ,
+    AST_EQUAL,
+
+    /* reserved words */
+    AST_INT,
+    AST_CHAR,
+    AST_GOTO,
+    AST_CONTINUE,
+    AST_BREAK,
+    AST_RETURN,
+    AST_FOR,
+    AST_DO,
+    AST_WHILE,
+    AST_IF,
+    AST_ELSE,
+    AST_SWITCH,
+    AST_CASE,
+    AST_DEFAULT,
+
+    /*
+     * invalid symbol that separates termainal and non-terminal values
+     */
+    AST_INVALID,
+
+    /*
+     * following variable values in grammar
+     */
     AST_CONSTANT,
     AST_PRIMARY_EXPRESSION,
     AST_POSTFIX_EXPRESSION,
@@ -62,63 +127,7 @@ enum astnode_t
     AST_DECLARATION,
     AST_FUNCTION_DEFINITION,
     AST_EXTERNAL_DECLARATION,
-    AST_TRANSLATION_UNIT,
-
-    /* tokens used in grammar rules */
-    AST_IDENTIFIER,
-    AST_ARROW,
-    AST_PLUS,
-    AST_PLUS_PLUS,
-    AST_PLUS_EQUAL,
-    AST_MINUS,
-    AST_MINUS_MINUS,
-    AST_MINUS_EQUAL,
-    AST_AMPERSAND,
-    AST_AMPERSAND_AMPERSAND,
-    AST_ASTERISK,
-    AST_ASTERISK_EQUAL,
-    AST_BACKSLASH,
-    AST_BACKSLASH_EQUAL,
-    AST_CARET,
-    AST_COMMA,
-    AST_MOD,
-    AST_MOD_EQUAL,
-    AST_QUESTIONMARK,
-    AST_COLON,
-    AST_SEMICOLON,
-    AST_LPAREN,
-    AST_RPAREN,
-    AST_LBRACKET,
-    AST_RBRACKET,
-    AST_LBRACE,
-    AST_RBRACE,
-    AST_VERTICALBAR,
-    AST_VERTICALBAR_VERTICALBAR,
-    AST_SHIFTLEFT,
-    AST_SHIFTRIGHT,
-    AST_LT,
-    AST_GT,
-    AST_LTEQ,
-    AST_GTEQ,
-    AST_EQ,
-    AST_NEQ,
-    AST_EQUAL,
-
-    /* reserved words */
-    AST_INT,
-    AST_CHAR,
-    AST_GOTO,
-    AST_CONTINUE,
-    AST_BREAK,
-    AST_RETURN,
-    AST_FOR,
-    AST_DO,
-    AST_WHILE,
-    AST_IF,
-    AST_ELSE,
-    AST_SWITCH,
-    AST_CASE,
-    AST_DEFAULT
+    AST_TRANSLATION_UNIT
 };
 
 struct astnode
