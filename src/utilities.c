ef7368ce165f75b4f09d6f7ca2820791bf6c2114
David Kimura <dkimura@pivotal.io>
2019-09-29 17:40:00 -0700

    Add parse table print debug function

--- src/utilities.c
+++ src/utilities.c
@@ -17,3 +17,48 @@ print_state(struct state *s)
         printf("  (%d,%d)\n", (int)(item->rewrite_rule-get_grammar()), item->cursor_position);
     }
 }
+
+void
+print_parsetable(struct parsetable_item *table)
+{
+    int i, j, num_states = 100; // FIXME: expose total states (state_identifier)..
+    struct parsetable_item *cell;
+
+    printf("parse table:\n");
+
+    for (i=0; i<NUM_SYMBOLS; i++)
+    {
+        printf("%3d ,", i);
+    }
+    printf("\n");
+    for (i=0; i<NUM_SYMBOLS; i++)
+    {
+        printf("-----");
+    }
+    printf("\n");
+
+    for (i=0; i<num_states; i++)
+    {
+        for (j=0; j<NUM_SYMBOLS; j++)
+        {
+            cell = table + (i * NUM_SYMBOLS) + j;
+            if (cell->shift)
+            {
+                printf("%3ds,", cell->state);
+            }
+            else if (cell->reduce)
+            {
+                printf("%3ldr,", cell->rule-get_grammar());
+            }
+            else if (cell->state)
+            {
+                printf("%3d ,", cell->state);
+            }
+            else
+            {
+                printf("    ,");
+            }
+        }
+        printf("\n");
+    }
+}
--- src/utilities.h
+++ src/utilities.h
@@ -4,4 +4,7 @@
 void
 print_state(struct state *state);
 
+void
+print_parsetable(struct parsetable_item *table);
+
 #endif
