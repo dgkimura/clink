82ce48c7c1c5a2c4a5b419f14879d87e9d0612f9
David Kimura <dkimura@pivotal.io>
2019-11-13 18:25:44 -0800

    Separate global and local symbol table

--- src/generator.c
+++ src/generator.c
@@ -6,8 +6,11 @@ struct symbol
     char *name;
 };
 
-int symbol_table_index = 0;
-struct symbol symbol_table[8192];
+int global_symbol_table_index = 0;
+struct symbol global_symbol_table[8192];
+
+int local_symbol_table_index = 0;
+struct symbol local_symbol_table[8192];
 
 static void *
 generate_code(struct astnode *ast)
