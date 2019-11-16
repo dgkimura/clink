11d660cbd82519ccf54ebe3d183546de448925b0
David Kimura <dkimura@pivotal.io>
2019-11-16 09:50:37 -0800

    Add struct and union declaration into symbol table

--- src/generator.c
+++ src/generator.c
@@ -5,7 +5,8 @@
 
 struct symbol
 {
-    char *name;
+    struct token *identifier;
+    enum token_t type;
 };
 
 int global_symbol_table_index = 0;
@@ -22,7 +23,7 @@ find_symbol(const char *name)
 
     for (i=0; i<global_symbol_table_index; i++)
     {
-        if (strcmp(global_symbol_table[i].name, name) == 0)
+        if (strcmp(global_symbol_table[i].identifier->value, name) == 0)
         {
             symbol = &global_symbol_table[i];
             break;
@@ -31,7 +32,7 @@ find_symbol(const char *name)
 
     for (i=0; i<local_symbol_table_index; i++)
     {
-        if (strcmp(local_symbol_table[i].name, name) == 0)
+        if (strcmp(local_symbol_table[i].identifier->value, name) == 0)
         {
             symbol = &local_symbol_table[i];
             break;
@@ -41,12 +42,29 @@ find_symbol(const char *name)
     return symbol;
 }
 
+struct astnode *
+get_node(struct listnode *children, enum astnode_t type)
+{
+    struct listnode *next;
+
+    next = children;
+    for (next=children; next!=NULL; next=next->next)
+    {
+        if (((struct astnode *)(next->data))->type == type)
+        {
+            return (struct astnode *)(next->data);
+        }
+    }
+
+    return NULL;
+}
+
 static void *
 generate_code(struct astnode *ast)
 {
     if (ast->type == AST_CONSTANT)
     {
-        return ast->constant;
+        return ast->token;
     }
 
     return NULL;
@@ -55,6 +73,25 @@ generate_code(struct astnode *ast)
 static void
 generate_symbol_table(struct astnode *ast)
 {
+    struct token *token;
+    enum token_t type;
+
+    if (ast->type == AST_STRUCT_OR_UNION_SPECIFIER)
+    {
+        token = get_node(ast->children, AST_IDENTIFIER)->token;
+        type = get_node(ast->children, AST_STRUCT_OR_UNION)->token->type;
+
+        if (token == NULL)
+        {
+            /* anonymous struct or union */
+            return;
+        }
+
+        global_symbol_table[global_symbol_table_index].identifier = token;
+        global_symbol_table[global_symbol_table_index].type = type;
+
+        global_symbol_table_index += 1;
+    }
 }
 
 void
