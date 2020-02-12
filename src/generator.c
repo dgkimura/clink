3b6ae5175f457e24cc4bb90a41037306baefa7f0
David Kimura <dkimura@pivotal.io>
2020-02-12 12:27:50 -0800

    Add generator visitor for cast expression

--- src/generator.c
+++ src/generator.c
@@ -529,9 +529,36 @@ visit_declaration_specifiers(struct astnode *ast, enum scope scope)
 }
 
 static void
+visit_unary_expression(struct astnode *ast, enum scope scope)
+{
+    assert(ast->type == AST_UNARY_EXPRESSION);
+}
+
+static void
 visit_cast_expression(struct astnode *ast, enum scope scope)
 {
+    struct listnode *list;
+    struct astnode *next;
+
     assert(ast->type == AST_CAST_EXPRESSION);
+
+    for (list=ast->children; list!=NULL; list=list->next)
+    {
+        next = (struct astnode *)list->data;
+        switch (next->type)
+        {
+            case AST_UNARY_EXPRESSION:
+            {
+                visit_unary_expression(next, scope);
+                break;
+            }
+            default:
+            {
+                assert(0);
+                break;
+            }
+        }
+    }
 }
 
 static void
