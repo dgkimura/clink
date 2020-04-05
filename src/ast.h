e9728953420dbfc983016b416298b598d3d9d306
David Kimura <dkimura@pivotal.io>
2020-04-05 13:34:22 -0700

    Move astnode structures into ast.h

--- src/ast.h
+++ src/ast.h
@@ -40,6 +40,154 @@
 #define CONST       0x1
 #define VOLATILE    0x2
 
+/*
+ * Following structs are nodes in the abstract syntax tree.
+ */
+
+struct ast_compound_statement
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    struct ast_declaration_list *declarations;
+    struct ast_statement_list *statements;
+};
+
+struct ast_statement_list
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    unsigned int size;
+    struct astnode *items[0];
+};
+
+struct ast_declaration_list
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    unsigned int size;
+    struct ast_declaration *items[0];
+};
+
+struct ast_declarator
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    int is_pointer;
+
+    char *declarator_identifier;
+
+    int declarator_value;
+
+    struct ast_parameter_type_list *declarator_parameter_type_list;
+
+    struct astnode *declarator_identifier_list;
+};
+
+struct ast_parameter_type_list
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    unsigned int size;
+    struct ast_declaration *items[0];
+};
+
+struct ast_declaration
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    struct token *token; /* XXX: Why is this needed? */
+
+    /*
+     * Storage specifiers - there are 5 specifiers in ast.h:
+     *     AUTO, REGISTER, STATIC, EXTERN, TYPEDEF
+     */
+    int storage_class_specifiers;
+
+    /*
+     * Type specifiers - there are 12 specifiers in ast.h:
+     *      void, char, short, long, ... typedef name
+     */
+    int type_specifiers;
+
+    /*
+     * Type qualifier - there are 2 specifiers:
+     *      const, volatile
+     */
+    int type_qualifier;
+
+    /*
+     * Declarators
+     */
+    int declarators_size;
+    struct ast_declarator *declarators[1];
+};
+
+struct ast_function
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    /*
+     * Contains specifiers and function args
+     */
+    struct ast_declarator *function_declarator;
+
+    /*
+     * List of variable declarations
+     */
+    struct astnode *declaration_list;
+
+    /*
+     * List of function statements
+     */
+    struct ast_compound_statement *statements;
+};
+
+struct ast_translation_unit
+{
+    enum astnode_t type;
+    enum astnode_t elided_type;
+
+    unsigned int translation_unit_items_size;
+
+    /*
+     * Variable length array of astnode with size specified by
+     * translation_unit_items.
+     */
+    struct astnode *translation_unit_items[0];
+};
+
+struct astnode
+{
+    enum astnode_t type;
+
+    /*
+     * An elided type is removes extra types that aren't necessary during
+     * generation.
+     */
+    enum astnode_t elided_type;
+    struct token *token;
+
+    union
+    {
+        int int_value;
+        char *identifier;
+
+        struct
+        {
+            enum astnode_t op;
+            struct astnode *left;
+            struct astnode *right;
+        };
+    };
+};
+
 struct astnode *
 create_translation_unit_node(struct listnode *list, struct rule *rule);
 
